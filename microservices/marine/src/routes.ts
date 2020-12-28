7d792a3e67b77fde33fc8ce7034d33df926fb3fc
Leos Julien <osx.leos@gmail.com>
2020-12-28 11:04:27 -0800

    Feat (marine): Add CRD for Server and Member

--- microservices/marine/src/routes.ts
+++ microservices/marine/src/routes.ts
@@ -1,2 +1,53 @@
+import { MemberController } from "./controller/MemberController";
+import { ServerController } from "./controller/ServerController";
+
 export const Routes = [
+    {
+        method: "get",
+        route: "/servers",
+        controller: ServerController,
+        action: "all",
+    },
+    {
+        method: "get",
+        route: "/servers/:serverId",
+        controller: ServerController,
+        action: "one",
+    },
+    {
+        method: "post",
+        route: "/servers",
+        controller: ServerController,
+        action: "add",
+    },
+    {
+        method: "delete",
+        route: "/servers/:serverId",
+        controller: ServerController,
+        action: "remove",
+    },
+    {
+        method: "get",
+        route: "/servers/:serverId/members",
+        controller: MemberController,
+        action: "all",
+    },
+    {
+        method: "get",
+        route: "/servers/:serverId/members/:memberId",
+        controller: MemberController,
+        action: "one",
+    },
+    {
+        method: "post",
+        route: "/servers/:serverId/members",
+        controller: MemberController,
+        action: "add",
+    },
+    {
+        method: "delete",
+        route: "/servers/:serverId/members/:memberId",
+        controller: MemberController,
+        action: "remove",
+    }
 ];
