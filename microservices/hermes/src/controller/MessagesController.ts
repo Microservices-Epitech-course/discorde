fd5a14eb95fd7fc1c7d2d6dbb2047b7e3fccd836
Henrixounez <henrix.ounez@yahoo.fr>
2020-12-29 17:45:15 +0100

    add: hermes: get/post/patch/delete routes and controllers

---
+++ microservices/hermes/src/controller/MessagesController.ts
@@ -0,0 +1,87 @@
+import { Channel, Message } from "@discorde/datamodel";
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+
+export class MessagesController {
+  private messageRepository = getRepository(Message);
+  private channelRepository = getRepository(Channel);
+
+  async findChannel(req: Request, res: Response) {
+    try {
+      const userRequestId = res.locals.jwtPayload.userId;
+      const channel = await this.channelRepository.findOneOrFail(req.params.id, { relations: ["messages", "messages.author", "channelRoleSettings", "channelRoleSettings.role", "server", "server.members", "server.members.user"]});
+      if (channel.server.members.findIndex((member) => member.user.id === userRequestId) === -1) {
+        res.status(404).send();
+        return null;
+      }
+      const member = channel.server.members.find((member) => member.user.id === res.locals.user.id);
+      return [channel, member];
+    } catch (e) {
+      res.status(404).send();
+      return null;
+    }
+  }
+
+  async get(req: Request, res: Response) {
+    const [channel, _] = await this.findChannel(req, res);
+
+    if (!channel)
+      return;
+    return channel.messages;
+  }
+
+  async post(req: Request, res: Response) {
+    const [channel, member] = await this.findChannel(req, res);
+
+    if (!channel)
+      return;
+    // TODO: Add media / mentions / ?
+    const { content } = req.body;
+    const message = this.messageRepository.create({
+      author: member,
+      channel: channel,
+      content: content,
+      media: null,
+    });
+    channel.messages.push(message);
+    this.messageRepository.save(message);
+    this.channelRepository.save(channel);
+    // TODO: Notify with WebSockets
+    res.status(200).send();
+  }
+
+  async patch(req: Request, res: Response) {
+    const [channel, member] = await this.findChannel(req, res);
+
+    if (!channel)
+      return;
+    const messageId = Number(req.params.messageId);
+    const message = channel.messages.find((message) => message.id === messageId);
+    if (!message || message.author.id !== member.id) {
+      res.status(404).send();
+      return;
+    }
+    // TODO: Add media / mentions / ?
+    const { content } = req.body;
+    message.content = content;
+    this.messageRepository.save(message);
+    // TODO: Notify with WebSockets
+    res.status(200).send();
+  }
+
+  async delete(req: Request, res: Response) {
+    const [channel, member] = await this.findChannel(req, res);
+
+    if (!channel)
+      return;
+    const messageId = Number(req.params.messageId);
+    const message = channel.messages.find((message) => message.id === messageId);
+    if (!message || message.author.id !== member.id) {
+      res.status(404).send();
+      return;
+    }
+    this.messageRepository.remove(message);
+    // TODO: Notify with WebSockets
+    res.status(200).send();
+  }
+}
