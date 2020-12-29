7ddea45227cd0b74b33d1d21bf224b79e40894fd
Henrixounez <henrix.ounez@yahoo.fr>
2020-12-29 17:45:15 +0100

    add: hermes: get/post/patch/delete routes and controllers

--- microservices/hermes/src/routes.ts
+++ microservices/hermes/src/routes.ts
@@ -1,2 +1,32 @@
+import { MessagesController } from "./controller/MessagesController";
+
 export const Routes = [
+  {
+    // Get Messages
+    method: "get",
+    route: "/channels/:id/messages",
+    controller: MessagesController,
+    action: "get",
+  },
+  {
+    // Send Message
+    method: "post",
+    route: "/channels/:id/messages",
+    controller: MessagesController,
+    action: "send",
+  },
+  {
+    // Modify Message
+    method: "patch",
+    route: "/channels/:id/messages/:messageId",
+    controller: MessagesController,
+    action: "patch",
+  },
+  {
+    // Delete Message
+    method: "delete",
+    route: "/channels/:id/messages/:messageId",
+    controller: MessagesController,
+    action: "delete",
+  },
 ];
