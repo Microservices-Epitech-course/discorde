88ebbc803c760c988faa2f122771f9e42468375e
Henrixounez <henrix.ounez@yahoo.fr>
2021-01-04 21:58:59 +0100

    fix: orm config to files because heroku monorepo

--- microservices/jbdm/src/index.ts
+++ microservices/jbdm/src/index.ts
@@ -4,7 +4,26 @@ import { createConnection } from "typeorm";
 import { Routes } from "./routes";
 import configServer from "@discorde/datamodel/lib/config/configServer";
 
-createConnection()
+createConnection({
+  "type": "postgres",
+  "url": process.env.DATABASE_URL,
+  "synchronize": true,
+  "logging": false,
+  "ssl": true,
+  "extra": {
+    "ssl": {
+      "rejectUnauthorized": false
+    }
+  },
+  "entities": ["node_modules/@discorde/datamodel/lib/entity/**/*"],
+  "migrations": ["src/migration/**/*.ts"],
+  "subscribers": ["src/subscriber/**/*.ts"],
+  "cli": {
+    "entitiesDir": "node_modules/@discorde/datamodel/lib/entity",
+    "migrationsDir": "src/migration",
+    "subscribersDir": "src/subscriber"
+  }
+})
   .then(async (connection) => {
     configServer(Number(process.env.PORT) || 3001, Routes, connection, "JBDM");
   })
