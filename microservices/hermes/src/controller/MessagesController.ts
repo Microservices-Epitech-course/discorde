3e93e3f824f7028f729284ee0bced7b5b7994c21
Henrixounez <henrix.ounez@yahoo.fr>
2021-01-05 08:37:53 +0100

    add: hermes: messages permissions

--- microservices/hermes/src/controller/MessagesController.ts
+++ microservices/hermes/src/controller/MessagesController.ts
@@ -1,4 +1,4 @@
-import { Channel, Message } from "@discorde/datamodel";
+import { Channel, Message, UserRole } from "@discorde/datamodel";
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
 import * as redis from "redis";
@@ -11,13 +11,18 @@ export class MessagesController {
   async findChannel(req: Request, res: Response) {
     try {
       const userRequestId = res.locals.jwtPayload.userId;
-      const channel = await this.channelRepository.findOneOrFail(req.params.id, { relations: ["messages", "messages.author", "channelRoleSettings", "channelRoleSettings.role", "server", "server.members", "server.members.user"]});
-      if (channel.server.members.findIndex((member) => member.user.id === userRequestId) === -1) {
+      const channel = await this.channelRepository.findOneOrFail(req.params.id, { relations: ["messages", "messages.author", "server"]});
+      const server = channel.server;
+      const member = await server.getUser(userRequestId);
+      if (!member && res.locals.user.role !== UserRole.ADMIN) {
         res.status(404).send();
         return null;
       }
-      const member = channel.server.members.find((member) => member.user.id === res.locals.user.id);
-      return [channel, member];
+      if (res.locals.user.role !== UserRole.ADMIN && !(await member.hasChannelPermission("viewChannels", channel.id))) {
+        res.status(404).send();
+        return null;
+      }
+      return {channel, member};
     } catch (e) {
       res.status(404).send();
       return null;
@@ -25,7 +30,7 @@ export class MessagesController {
   }
 
   async get(req: Request, res: Response) {
-    const [channel, _] = await this.findChannel(req, res);
+    const {channel } = await this.findChannel(req, res);
 
     if (!channel)
       return;
@@ -33,10 +38,14 @@ export class MessagesController {
   }
 
   async post(req: Request, res: Response) {
-    const [channel, member] = await this.findChannel(req, res);
+    const {channel, member} = await this.findChannel(req, res);
 
     if (!channel)
       return;
+    if (!(await member.hasChannelPermission("sendMessages", channel.id))) {
+      res.status(404).send();
+      return;
+    }
     // TODO: Add media / mentions / ?
     const { content } = req.body;
     const message = this.messageRepository.create({
@@ -46,44 +55,53 @@ export class MessagesController {
       media: null,
     });
     channel.messages.push(message);
-    this.messageRepository.save(message);
     this.channelRepository.save(channel);
     this.publisher.publish(`channel:${channel.id}`, JSON.stringify({action: "messageAdd", data: message}));
-    res.status(200).send();
+    return this.messageRepository.save(message);
   }
 
   async patch(req: Request, res: Response) {
-    const [channel, member] = await this.findChannel(req, res);
+    const {channel, member} = await this.findChannel(req, res);
 
     if (!channel)
       return;
     const messageId = Number(req.params.messageId);
     const message = channel.messages.find((message) => message.id === messageId);
-    if (!message || message.author.id !== member.id) {
+    if (!message) {
       res.status(404).send();
       return;
+    }
+    if (message.author.id !== member.id && res.locals.user.role !== UserRole.ADMIN) {
+      if (!(await member.hasChannelPermission("manageMessages", channel.id))) {
+        res.status(404).send();
+        return;
+      }
     }
     // TODO: Add media / mentions / ?
     const { content } = req.body;
     message.content = content;
-    this.messageRepository.save(message);
     this.publisher.publish(`channel:${channel.id}`, JSON.stringify({action: "messageUpdate", data: message}));
-    res.status(200).send();
+    return this.messageRepository.save(message);
   }
 
   async delete(req: Request, res: Response) {
-    const [channel, member] = await this.findChannel(req, res);
+    const {channel, member} = await this.findChannel(req, res);
 
     if (!channel)
       return;
     const messageId = Number(req.params.messageId);
     const message = channel.messages.find((message) => message.id === messageId);
-    if (!message || message.author.id !== member.id) {
+    if (!message) {
       res.status(404).send();
       return;
     }
-    this.messageRepository.remove(message);
+    if (message.author.id !== member.id && res.locals.user.role !== UserRole.ADMIN) {
+      if (!(await member.hasChannelPermission("manageMessages", channel.id))) {
+        res.status(404).send();
+        return;
+      }
+    }
     this.publisher.publish(`channel:${channel.id}`, JSON.stringify({action: "messageDelete", data: messageId}));
-    res.status(200).send();
+    return this.messageRepository.remove(message);
   }
 }
