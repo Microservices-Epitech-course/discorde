9f1462ecc102999bc8bcdbce80558e5043f75a43
Henrixounez <henrix.ounez@yahoo.fr>
2021-01-05 08:22:43 +0100

    add: datamodel: Role + ChannelRole settings / Member owner + quit booleans + hasPermissions function / Server name field + fix has/get user/member functions

--- microservices/datamodel/src/entity/Member.ts
+++ microservices/datamodel/src/entity/Member.ts
@@ -1,5 +1,6 @@
-import { Entity, ManyToMany, CreateDateColumn, UpdateDateColumn, PrimaryGeneratedColumn, OneToMany, ManyToOne, BeforeRemove, getRepository } from "typeorm";
+import { Entity, ManyToMany, CreateDateColumn, UpdateDateColumn, PrimaryGeneratedColumn, OneToMany, ManyToOne, BeforeRemove, getRepository, Column } from "typeorm";
 import { User, Role, Server, ServerType, Message, Reaction } from "./";
+import { ChannelRoleSettingsModifier } from "./ChannelRoleSettings";
 
 @Entity()
 export class Member {
@@ -21,6 +22,12 @@ export class Member {
   @ManyToMany(type => Reaction, reaction => reaction.members)
   reactions: Reaction[];
 
+  @Column({default: false})
+  owner: boolean;
+
+  @Column({default: false})
+  quit: boolean;
+
   @CreateDateColumn()
   createdAt: Date;
   @UpdateDateColumn()
@@ -34,5 +41,54 @@ export class Member {
     await getRepository(Reaction).remove(member.reactions);
     if (member.server.type === ServerType.CONVERSATION && member.server.members.length === 1)
       await getRepository(Server).remove(member.server);
+  }
+
+  async isAdmin() {
+    const member = await getRepository(Member).findOne(this.id, { relations: ['roles']});
+    if (member.owner)
+      return true;
+    return member.roles.reduce((acc, e) => {
+      if (e.administrator)
+        acc = true;
+      return acc;
+    }, false);
+  }
+  async hasGlobalPermission(permission: string) {
+    const member = await getRepository(Member).findOne(this.id, { relations: ['roles']});
+    if (await member.isAdmin())
+      return true;
+    return member.roles.reduce((acc, e) => {
+      if (e[permission])
+        acc = true;
+      return acc;
+    }, false);
+  }
+  async hasChannelPermission(permission: string, channelId: number) {
+    const globalPerm = await this.hasGlobalPermission(permission);
+    const member = await getRepository(Member).findOne(channelId, { relations: ['roles', 'roles.channelRoleSettings', 'roles.channelRoleSettings.channel']});
+
+    if (await member.isAdmin())
+      return true;
+
+    const channelRoleSettings = member.roles.map(role => ({...role, channelRoleSettings: role.channelRoleSettings.filter(channelRole => channelRole.channel.id === channelId)}));
+    const channelPerm = channelRoleSettings.reduce((acc, e) => {
+      if (acc === 1)
+        return acc;
+      let value = 0;
+      if (e.channelRoleSettings.length === 1 && e.channelRoleSettings[0][permission] !== ChannelRoleSettingsModifier.DEFAULT) {
+        if (e.channelRoleSettings[0][permission] === ChannelRoleSettingsModifier.ALLOW)
+          value = 1;
+        else
+          value = -1;
+      }
+      if (value > acc)
+        return value;
+      return acc;
+    }, 0);
+    if ((globalPerm && channelPerm === -1) || (!globalPerm && channelPerm <= 0))
+      return false;
+    else if ((globalPerm && channelPerm >= 0) || (!globalPerm && channelPerm === 1))
+      return true;
+    return false;
   }
 }
--- microservices/datamodel/src/entity/Server.ts
+++ microservices/datamodel/src/entity/Server.ts
@@ -11,6 +11,9 @@ export class Server {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column()
+  name: string;
+
   @OneToMany(type => Member, member => member.server)
   members: Member[];
 
@@ -42,9 +45,23 @@ export class Server {
     await getRepository(Invitation).remove(server.invitations);
   }
 
-  hasMember(userId: number) {
-    if (this.members.find(member => member.user.id === userId))
-      return true;
-    return false;
+  async hasUser(userId: number) {
+    const server = await getRepository(Server).findOne(this.id, { relations: ['members'] });
+    return server.members.findIndex(member => member.user.id === userId && !member.quit) !== -1;
+  }
+  hasMember(memberId: number) {
+    return this.members.findIndex(member => member.id === memberId && !member.quit) !== -1;
+  }
+  async getUser(userId: number) {
+    const server = await getRepository(Server).findOne(this.id, { relations: ['members'] });
+    return server.members.find(member => member.user.id === userId && !member.quit);
+  }
+  async getMember(memberId: number) {
+    const server = await getRepository(Server).findOne(this.id, { relations: ['members'] });
+    return server.members.find(member => member.id === memberId && !member.quit);
+  }
+  async getEveryoneRole() {
+    const server = await getRepository(Server).findOne(this.id, { relations: ['roles'] });
+    return server.roles.find(role => role.isEveryone === true);
   }
 }
