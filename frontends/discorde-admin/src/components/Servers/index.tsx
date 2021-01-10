dfedbfc2fb9a5918d2951a2c136fa9e70d872061
Juel <juel.marques-garcia@epitech.eu>
2021-01-10 18:48:48 +0100

    feat: add servers fetching

---
+++ frontends/discorde-admin/src/components/Servers/index.tsx
@@ -0,0 +1,64 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { Server } from 'utils/api';
+import { marine } from 'utils/api/client';
+import { useRouter } from 'next/router';
+import { Container, Item } from './style';
+
+const ServersSelector: React.FC = () => {
+  const router = useRouter();
+  const [servers, setServers] = useState<Server[]>(undefined);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    marine
+      .get('/servers')
+      .then(({ status, data }) => {
+        setServers(data);
+      })
+      .catch((err) => {
+        console.error(err);
+        setServers(null);
+      });
+  }, []);
+
+  const handleSelection = useCallback(
+    (server: Server): void => {
+      router
+        .push({
+          pathname: `/servers/${server.id}`,
+        })
+        .then(() => {})
+        .catch((err) => {
+          console.error(err);
+        });
+    },
+    [router],
+  );
+
+  if (servers === undefined) return <div>Loading...</div>;
+  if (servers === null) return <div>Something goes wrong...</div>;
+  return (
+    <Container>
+      <div id="header">
+        Search:
+        <input placeholder="search" onChange={({ target: { value } }) => setFilter(value)} />
+      </div>
+      <div id="selection">
+        <div id="selectionHeader">
+          <div>Name</div>
+          <div>Type</div>
+        </div>
+        {servers
+          .filter(({ name }) => name.includes(filter))
+          .map((server) => (
+            <Item onClick={() => handleSelection(server)}>
+              <div>{server.name}</div>
+              <div>{server.type}</div>
+            </Item>
+          ))}
+      </div>
+    </Container>
+  );
+};
+
+export default ServersSelector;
